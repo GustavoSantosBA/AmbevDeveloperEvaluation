f4c9db8b8740677272f9036d012e51c02c2a3138
GustavoSantosBA <95386548+GustavoSantosBA@users.noreply.github.com>
2025-07-16 20:27:19 -0300

    implement ORM features

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.SaleNumber).IsRequired();
+            builder.Property(x => x.SaleDate).IsRequired();
+            builder.Property(x => x.Customer).IsRequired();
+            builder.Property(x => x.Branch).IsRequired();
+            builder.Property(x => x.TotalAmount).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Status).IsRequired();
+
+            builder.HasMany(x => x.Items)
+                   .WithOne()
+                   .HasForeignKey(x => x.SaleId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+    public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Product).IsRequired();
+            builder.Property(x => x.Quantity).IsRequired();
+            builder.Property(x => x.UnitPrice).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Discount).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Total).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(x => x.IsCancelled).IsRequired();
+        }
+    }
+}
